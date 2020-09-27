a02ae9a9fed5ad404a838da1701b6368f1b9e742
Hanna Murai <annmuray75@gmail.com>
2020-09-27 15:22:31 -0500

    calendar

--- script.js
+++ script.js
@@ -1,32 +1,63 @@
 const date = new Date();
 
-const month = date.getMonth();
-date.setDate(1);
-console.log("Day", date.getDay());
-console.log(month);
+const render = () => {
+  const month = date.getMonth();
+  date.setDate(1);
 
-const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December"
-];
-let lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
-let firstDayIndex = date.getDay();
-document.querySelector(".date h1").innerHTML = months[date.getMonth()];
-document.querySelector(".date p").innerHTML = date.toDateString();
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+  let lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
 
-let days = "";
-let monthDays = document.querySelector(".days");
-for (let i = 1; i <= lastDay; i++) {
-  days += `<div>${i}</div>`;
-}
-monthDays.innerHTML = days;
+  let firstDayIndex = date.getDay();
+  let lastDayIndex = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDay();
+  console.log("last", lastDayIndex);
+  document.querySelector(".date h1").innerHTML = months[date.getMonth()];
+  document.querySelector(".date p").innerHTML = date.toDateString();
+  let prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+  console.log(prevLastDay);
+  let days = "";
+  for (let x = firstDayIndex; x > 0; x--) {
+    days += `<div class='prev-date'>${prevLastDay - x + 1}</div>`;
+  }
+  let monthDays = document.querySelector(".days");
+  for (let i = 1; i <= lastDay; i++) {
+    if (
+      i === new Date().getDate() &&
+      date.getMonth() === new Date().getMonth()
+    ) {
+      days += `<div class='today'>${i}</div>`;
+    }
+    days += `<div >${i}</div>`;
+  }
+  for (let i = 1; i < 6 - lastDayIndex; i++) {
+    days += `<div class='next-date'>${i}</div>`;
+  }
+  monthDays.innerHTML = days;
+};
+let prev = document.querySelector(".prev");
+let next = document.querySelector(".next");
+prev.onclick = () => {
+  date.setMonth(date.getMonth() - 1);
+  render();
+};
+next.onclick = () => {
+  date.setMonth(date.getMonth() + 1);
+  render();
+};
+render();

f3843e41df7001037bea64d9a4a3c4df33623f31
Hanna Murai <annmuray75@gmail.com>
2020-10-02 14:55:37 -0500

    backend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
-
+require("dotenv").config();
 const app = express();
 const port = 3001;
 const mongoose = require("mongoose");
 const MONGODB_URI = process.env.MONGODB_URI;
+
 console.log("Connecting DB to ", MONGODB_URI);
 
 mongoose
@@ -12,8 +13,6 @@ mongoose
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
     );
-    let data1 = await List.insertMany(list);
-    console.log(data1);
   })
   .catch(err => console.error("Error connecting to mongo", err));
 

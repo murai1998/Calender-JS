f2c9f974d913905ccc0d1b0f59b99f1f050770e5
Hanna Murai <annmuray75@gmail.com>
2020-10-03 12:17:33 -0500

    app

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,16 @@
 const express = require("express");
 require("dotenv").config();
 
+const app = express();
 const port = 3001;
 const mongoose = require("mongoose");
 const MONGODB_URI = process.env.MONGODB_URI;
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const path = require("path");
-const cors = require("cors");
-const session = require("express-session");
-const logger = require("morgan");
+
 console.log("Connecting DB to ", MONGODB_URI);
+
+app.get("/", (req, res) => {
+  res.send("This is home");
+});
 
 mongoose
   .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -21,33 +21,8 @@ mongoose
   })
   .catch(err => console.error("Error connecting to mongo", err));
 
-const app = express();
-
-app.use(
-  cors({
-    credentials: true,
-    origin: ["http://localhost:3001", "https://clientnetlify.netlify.app"] //Swap this with the client url
-  })
-);
-
-app.use(
-  session({
-    resave: false,
-    saveUninitialized: true,
-    secret: "secret",
-    cookie: { maxAge: 1000 * 60 * 60 }
-  })
-);
-
-app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(logger("dev"));
 const index = require("./routes/index");
 
 app.use("/api", index);
-app.get("*", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
-});
+
+app.listen(3001);
